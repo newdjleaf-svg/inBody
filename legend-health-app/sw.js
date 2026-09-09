a086ab73870907d2b0f7fc0aaab7e0a627033f51
newdjleaf-svg <newdjleaf@gmail.com>
2026-09-09 22:09:12 +0800

    Add files via upload

--- legend-health-app/sw.js
+++ legend-health-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='legend-health-v11';
+const CACHE='legend-health-v12';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/logo.jpeg'];
 
 self.addEventListener('install',e=>{
